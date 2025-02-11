bd5e47f1cc9c74e9cc87f16a8ee85b87397d7781
Divyansh Jain <divyansh17jain@gmail.com>
2025-02-11 20:31:13 +0530

    Enhance login with authentiction

--- login.py
+++ login.py
@@ -7,4 +7,6 @@ if username == "admin" and password == "admin123":
         print("Login Successful")
     else:
         print("Invalid Credentials")
-        
+       
+login ("admin". "admin123")
+
